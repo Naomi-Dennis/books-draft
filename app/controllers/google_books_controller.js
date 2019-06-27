1e48644c60a14ff00dc5cd8895f541cdb1ae1597
Naomi-Dennis <njdennis94@gmail.com>
2019-06-26 21:41:15 -0400

    Add private is_logged_in Google Books Controller, auth action, index action, and pass is_logged_in() result to index

--- app/controllers/google_books_controller.js
+++ app/controllers/google_books_controller.js
@@ -1,8 +1,24 @@
 /* 
  * Google Books Controller
  * */
+
 module.exports = {
    index: (req, res) => {
-      res.render('layout', {view_path: 'google_books/index' });
- }
+      res.render('layout', {view_path: 'google_books/index', is_logged_in: is_logged_in() });
+ },
+ index_logic: (req, res, next) =>{
+	 next(); 
+ },
+ create_user: (req, res) => {
+	 console.log("REQUEST": req);
+	 console.log("RESPONSE": res); 
+	access_token = req.query.access_token
+	global.session = access_token
+	res.redirect("/")
+   }
+ }//end module
+
+function is_logged_in(){
+ return (global.session != "NOT SET")
 }
+

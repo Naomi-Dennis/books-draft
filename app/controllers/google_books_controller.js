4d818f947aa278decc5761eb7f4068e50f3306e0
Naomi-Dennis <njdennis94@gmail.com>
2019-07-02 02:03:33 -0400

    Restructure authentication to use server-side version of google auth2

--- app/controllers/google_books_controller.js
+++ app/controllers/google_books_controller.js
@@ -4,16 +4,20 @@
 
 module.exports = {
    index: (req, res) => {
-      res.render('layout', {view_path: 'google_books/index', is_logged_in: is_logged_in() });
+      if(!is_logged_in()){
+	url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${options.client_id}&response_type=code&scope=${options.scope}&redirect_uri=${options.redirect_uri}`;
+	res.redirect(url)
+      }
+      else{ 
+	res.render('layout', {view_path: 'google_books/index'}); 
+      }
  },
  index_logic: (req, res, next) =>{
 	 next(); 
  },
  create_user: (req, res) => {
-	//access_token = req.params
-	 console.log("CREATE USER REQUEST", req);
-	 console.log("CREATE USER RESPONSE", res);
-	//global.session = access_token
+	access_token = res.query.code
+	global.session = access_token
 	res.redirect("/")
    },
 
@@ -24,5 +28,14 @@ module.exports = {
 
 function is_logged_in(){
  return (global.session != "NOT SET" && global.session != {} && global.session != "")
+}
+
+let test_domain = "http://localhost:3000/auth"
+let heroku_domain = "https://stark-wave-13030.herokuapp.com/auth" 
+let options = {
+	client_secret: "7h0r3G1m-JITodmlYl2Fj5YV",
+	client_id: "73107975855-dapdgln79j62ovmt1kf2ootsv5rb9mhf.apps.googleusercontent.com",
+	scope: "email",
+	redirect_uri: test_domain
 }
 

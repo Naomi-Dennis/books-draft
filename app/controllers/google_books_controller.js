96688fb0881099ed6178160145123119e11f6439
Naomi-Dennis <njdennis94@gmail.com>
2019-07-05 15:05:00 -0400

    Refractor query_book() action and log errors

--- app/controllers/google_books_controller.js
+++ app/controllers/google_books_controller.js
@@ -9,27 +9,49 @@ module.exports = {
 	res.redirect(url)
       }
       else{
-	res.render('layout', {view_path: 'google_books/index', searched_books: module.exports.searched_books}); 
+	let view_params = {
+		view_path: "google_books/index",
+		searched_books: module.exports.searched_books
+	}
+	console.log( "selected books", module.exports.searched_books.length )	
+	res.render('layout', view_params); 
       }
  },
   query_book: (req, res) => {
 	searched_books = []
 	data = req.query
-	title = data.book_title.replace(/ /g, '-')
-	author = data.book_author.replace(/ /g, '-')
+	regex_test = /[A-z0-9']/g
+	title =  (data.book_title || "") 
+	author = (data.book_author || "") 
+
 	console.log("Title", title)
-	console.log("Author", author);
-	search_url = `https://www.googleapis.com/books/v1/volumes?q=intitle:${title}&inauthor:${author}` 
-	axios.get(search_url).then( (response) => {
-		module.exports.searched_books = response.data.items;
-			console.log(module.exports.searched_books)
-		res.redirect("/")
-	}
-	).catch( (error) => { console.log(error);  } )
+	console.log("Author", author);	
+
+	query = ""
+	query_params = []
+	query_params.push(validate_query(title, "title"))
+	query_params.push(validate_query(author, "author"))
+        query = query_params.filter(String).join("&")
+	query = query.length > 0 ? `${query}&maxResults=40` : query
+	console.log("Query", query)
+		search_url = `https://www.googleapis.com/books/v1/volumes?q=${query}`
+		console.log(search_url)
+		axios.get(search_url)
+		.then( (response) => {
+				module.exports.searched_books = response.data.items || [];
+				console.log( "selected books", module.exports.searched_books.length )
+				res.redirect("/")
+		}).catch((error) => { 
+			console.log("Error", error.message)
+			res.redirect("/")
+		})
   },
 searched_books: [] 
  }//end module
 
+function validate_query( metric, key ){
+  return metric.length > 0 ? `in${key}:${metric.replace(/ /g, '-')}` : ""
+}
 
 function is_logged_in(){
  return (global.session != "NOT SET" && global.session != {} && global.session != "")
@@ -41,6 +63,6 @@ let options = {
 	client_secret: "7h0r3G1m-JITodmlYl2Fj5YV",
 	client_id: "73107975855-dapdgln79j62ovmt1kf2ootsv5rb9mhf.apps.googleusercontent.com",
 	scope: "email",
-	redirect_uri: heroku_domain
+	redirect_uri: test_domain
 }
 

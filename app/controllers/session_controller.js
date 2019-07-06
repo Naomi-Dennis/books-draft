47ae90f21194bb112f56de821aa5f332707950c4
Naomi-Dennis <njdennis94@gmail.com>
2019-07-05 20:52:38 -0400

    Refactor session controller

--- app/controllers/session_controller.js
+++ app/controllers/session_controller.js
@@ -9,11 +9,13 @@ module.exports = {
       axios.post("https://www.googleapis.com/oauth2/v4/token", {
 	client_id: options.client_id,
 	client_secret: options.client_secret,
-	redirect_uri: heroku_domain,
+	redirect_uri: current_domain,
 	code: access_code,
 	grant_type: "authorization_code"
-      }).then( module.exports.get_authorization_code ).catch( module.exports.display_error );
-      res.redirect("/")
+      }).then( (data) => {
+      	module.exports.get_authorization_code(data); 
+	res.redirect("/")
+      }).catch( module.exports.display_error );
    },
    get_authorization_code: (res) => {
 	access_token = res.data.access_token
@@ -26,11 +28,12 @@ module.exports = {
 
 let test_domain = "http://localhost:3000/auth"
 let heroku_domain = "https://stark-wave-13030.herokuapp.com/auth" 
+let current_domain = test_domain
 let options = {
 	client_secret: "7h0r3G1m-JITodmlYl2Fj5YV",
 	client_id: "73107975855-dapdgln79j62ovmt1kf2ootsv5rb9mhf.apps.googleusercontent.com",
 	scope: "email",
-	redirect_uri: heroku_domain
+	redirect_uri: current_domain
 
 }
 

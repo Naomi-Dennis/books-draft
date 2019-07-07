90653c8b8028ff112e52307a8b4e059bf85f95c4
Naomi-Dennis <njdennis94@gmail.com>
2019-07-06 22:29:04 -0400

    Remove server configurations from sessions and google_books controller; change server config reference to global.config

--- app/controllers/session_controller.js
+++ app/controllers/session_controller.js
@@ -7,9 +7,9 @@ module.exports = {
    process_code: (req, res) => {
       access_code = req.query.code
       axios.post("https://www.googleapis.com/oauth2/v4/token", {
-	client_id: options.client_id,
-	client_secret: options.client_secret,
-	redirect_uri: current_domain,
+	client_id: global.config.client_id,
+	client_secret: global.config.client_secret,
+	redirect_uri: global.config.redirect_uri,
 	code: access_code,
 	grant_type: "authorization_code"
       }).then( (data) => {
@@ -24,16 +24,5 @@ module.exports = {
    display_error: (error) => {
 	console.log(`*******Authentication error encountered***********\n${error.message}\nRefreshing Page...`)
    }
-}
-
-let test_domain = "http://localhost:3000/auth"
-let heroku_domain = "https://stark-wave-13030.herokuapp.com/auth" 
-let current_domain = test_domain
-let options = {
-	client_secret: "7h0r3G1m-JITodmlYl2Fj5YV",
-	client_id: "73107975855-dapdgln79j62ovmt1kf2ootsv5rb9mhf.apps.googleusercontent.com",
-	scope: "email",
-	redirect_uri: current_domain
-
 }
 

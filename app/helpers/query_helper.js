498d81a0da339925c18a0ce7d08bf6b1c40ddcb2
Naomi-Dennis <njdennis94@gmail.com>
2019-07-08 16:05:50 -0400

    Change query_book#query to set searched_books only if items is defined

--- app/helpers/query_helper.js
+++ app/helpers/query_helper.js
@@ -17,7 +17,7 @@ module.exports = {
 	   axios.get(query)
 	   .then( (response) => {
 		if(!return_searched_books){ 
-			module.exports.process_searched_books( response.data.items) ;
+			(response.data.items) ? module.exports.process_searched_books( response.data.items) : ""
 		}
 		else{
 			return response.data.items
@@ -36,6 +36,7 @@ module.exports = {
 	},
 	process_searched_books: (items ) => {
 		module.exports.searched_books = []
+		
 		if( items || items.length > 0){
 			items.forEach((item_element) => { 
 				module.exports.searched_books.push( new Book(item_element) );

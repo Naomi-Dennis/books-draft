0d7f78bd4ee4afa4ee881473b5f737b345eb70d3
Naomi-Dennis <njdennis94@gmail.com>
2019-07-08 15:38:45 -0400

    Add alternative to book description if textSnippet is not defined

--- app/helpers/book.js
+++ app/helpers/book.js
@@ -6,7 +6,7 @@ class Book{
 		this.authors = item.volumeInfo.authors ? item.volumeInfo.authors.join(", ") : "No Authors Found"
 		this.publisher = item.volumeInfo.publisher || "No Publisher Found"
 		this.publisherDate = item.volumeInfo.publishedDate ? item.volumeInfo.publishedDate.split("-")[0] : "No Published Date Found"
-		this.description = item.searchInfo.textSnippet
+		this.description = item.searchInfo ? item.searchInfo.textSnippet : item.volumeInfo.description
 		this.image = item.volumeInfo.imageLinks.smallThumbnail || volumeInfo.imageLinks.thumbnail || "https://via.placeholder.com/600.png?text=Cover"
 	}
 }

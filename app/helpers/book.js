62fe14947618186831632ee939c535d5080f0c9a
Naomi-Dennis <njdennis94@gmail.com>
2019-07-08 14:43:18 -0400

    Add Book class

---
+++ app/helpers/book.js
@@ -0,0 +1,14 @@
+class Book{
+	constructor(item){
+		this.id = item.id
+		this.title = item.volumeInfo.title
+		this.subtitle = item.volumeInfo.subtitle || ""
+		this.authors = item.volumeInfo.authors ? item.volumeInfo.authors.join(", ") : "No Authors Found"
+		this.publisher = item.volumeInfo.publisher || "No Publisher Found"
+		this.publisherDate = item.volumeInfo.publishedDate ? item.volumeInfo.publishedDate.split("-")[0] : "No Published Date Found"
+		this.description = item.searchInfo.textSnippet
+		this.image = item.volumeInfo.imageLinks.smallThumbnail || volumeInfo.imageLinks.thumbnail || "https://via.placeholder.com/600.png?text=Cover"
+	}
+}
+
+module.exports = Book
